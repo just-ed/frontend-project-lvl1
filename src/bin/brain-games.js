0abb2cd762ae546a0bd86f359b701287c0e500a7
just-ed <ed_tomeyan@mail.ru>
2020-01-11 18:58:36 +0300

    brain-even game implemented

--- src/bin/brain-games.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-import hello from '..';
-
-console.log("Welcome to the Brain Games!");
-console.log(hello());
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-import hello from './hello';
+import readlineSync from 'readline-sync';
+import * as brainEven from './brain-even';
 
-export default hello;
+const playGame = () => {
+  console.log("Welcome to the Brain Games!");
+  console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
+  
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+
+  const numOfQuestions = 3;
+  let currentQuestion = 0;
+
+  while (currentQuestion < numOfQuestions) {
+    const num = Math.floor(Math.random() * 100);
+    console.log(`Question: ${num}`);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    if (!brainEven.isCorrectAnswer(answer, num)) {
+      return `'${answer}' is wrong answer ;(.` +
+        `Correct answer was '${brainEven.correctAnswer(num)}'.` +
+        `\nLet's try again, ${name}!`
+    }
+
+    console.log('Correct!');
+    currentQuestion += 1;
+  }
+
+  return `Congratulations, ${name}!`;
+};
+
+export default playGame;
